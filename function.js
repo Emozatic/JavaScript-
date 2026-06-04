6e586aa658ae7fa4ac1eb95a411e066a9d18beab
Lucky Sah <lk4179869@gmail.com>
2026-06-04 20:10:18 +0530

    add: functions chapter

--- function.js
+++ function.js
@@ -14,6 +14,32 @@ function greet(name){
 
 greet("Lucky");
 
-(greet(name){
-    return `namaste ${name}`;
-})()
+//task 1:-
+
+function checkEven(num){
+    return num%2==0 ? true : false;
+}
+
+console.log(checkEven(4));
+console.log(checkEven(5));
+
+
+//task 2:-
+
+function table(num){
+    for(let i=1; i<=10; i++){
+        console.log(`${num}*${i}=${num*i}`);
+    }
+}
+
+
+//mini project
+
+function calator(a,b){
+    console.log(a+b);
+    console.log(a-b)
+    console.log(a*b)
+    console.log(a/b)
+}
+
+calator(10,5)

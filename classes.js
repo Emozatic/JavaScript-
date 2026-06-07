f0bd787851f44125a5752900837cace8fad4d05d
Lucky Sah <lk4179869@gmail.com>
2026-06-07 17:42:19 +0530

    classes and object

---
+++ classes.js
@@ -0,0 +1,12 @@
+//---------------CLASSES------------------
+
+class Student{
+    constructor(name,age){
+        this.name= name
+        this.age=age
+    }
+
+}
+
+let s1= new Student("Lucky",121)
+console.log(s1.name);
